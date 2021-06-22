64252eda9aabb613a1319cd8d46423a360c3feea
lsw1104 <tomy807@naver.com>
2021-06-22 19:28:33 +0900

    게임 시작 구현

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Game {
     public void play(){
         System.out.println("=======BlackJack=======");
+        Scanner sc= new Scanner(System.in);
         Dealer dealer=new Dealer();
         Gamer gamer=new Gamer();
         Rule rule=new Rule();
         CardDeck cardDeck=new CardDeck();
-        System.out.println(cardDeck.toString());
+//        System.out.println(cardDeck.toString());
+        initPhase(cardDeck,gamer);
+        playingPhase(sc,cardDeck,gamer);
+    }
+    private static final int INIT_RECEIVE_CARD_COUNT=2;
+    private void initPhase(CardDeck cardDeck, Gamer gamer) {
+        System.out.println("처음 2장의 카드를 각자 뽑겠습니다.");
+        for(int i=0;i<INIT_RECEIVE_CARD_COUNT;i++){
+            Card card=cardDeck.draw();
+            gamer.receiveCard(card);
+        }
+    }
+
+    private void playingPhase(Scanner sc,CardDeck caedDeck,Gamer gamer){
+        String gamerInput;
+        while(true){
+            System.out.println("카드를 뽑겠습니까? 종료를 원하시면 0을 입력하세요.");
+            gamerInput=sc.nextLine();
+            if("0".equals(gamerInput)){
+                break;
+            }
+            Card card=caedDeck.draw();
+            gamer.receiveCard(card);
+        }
+
     }
 }
